9eba8a5c82c0d269bdfb148276aa1aa22bb5ba30
DFI-named-ION <larityandselnter@gmail.com>
2024-04-26 12:49:55 -0400

    small changes: +axios +dotenv +bootstrap +prepare for workflow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*		code goes here		*/}
+            <h1 className="text-center text-light bg-dark w-25 mx-auto mt-5">HALO MA FRAND</h1>
+        </div>
+    );
 }
 
 export default App;
