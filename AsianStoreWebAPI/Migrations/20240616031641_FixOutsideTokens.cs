20e96cb38b6119751fc67a5a13558f819f35dab2
DFI-named-ION <larityandselnter@gmail.com>
2024-06-15 23:17:29 -0400

    FixedOutsideTokens + Migration

---
+++ AsianStoreWebAPI/Migrations/20240616031641_FixOutsideTokens.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AsianStoreWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixOutsideTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountId",
+                table: "GoogleTokens",
+                type: "nvarchar(MAX)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountId",
+                table: "FacebookTokens",
+                type: "nvarchar(MAX)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountId",
+                table: "AppleTokens",
+                type: "nvarchar(MAX)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "GoogleTokens");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "FacebookTokens");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "AppleTokens");
+        }
+    }
+}
