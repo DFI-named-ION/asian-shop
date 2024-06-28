fafd530cd9ccb42970fc8ea0ec6de87d6e55a476
DFI-named-ION <larityandselnter@gmail.com>
2024-06-28 03:33:05 -0400

    ~hook

--- src/component/hooks/AcceptRedirect.js
+++ src/component/hooks/AcceptRedirect.js
@@ -1,4 +1,9 @@
 export default function MailConfirmation() {
-    console.log(new URLSearchParams(window.location.search));
     console.log("Hooked!");
+
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log(`Count of parameters: ${urlParams.toString().split('&').length}`);
+    for (const [key, value] of urlParams.entries()) {
+        console.log(`${key}: ${value}`);
+    };
 };
