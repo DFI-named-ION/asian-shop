f4931eab1dc7c6ec39dbb4db55dc7a8ce3a7ef80
xoma <vladkhomenko489@gmail.com>
2024-07-05 19:35:23 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import HomePage from "./component/PasswordChange"
+import HomePage from "./component/HomePage"
 import AuthorizationPage from './component/Authorization';
 import RegistrationPage from "./component/Registration";
 import MailConfirmationPage from "./component/PasswordChange";
