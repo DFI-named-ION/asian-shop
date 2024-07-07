9f2f53f94eda1e8d8f0673948a2b9afeed18fc83
DFI-named-ION <larityandselnter@gmail.com>
2024-07-07 07:37:47 -0400

    small routes fixes

--- src/component/Registration.js
+++ src/component/Registration.js
@@ -91,12 +91,7 @@ export default function Registration() {
 
     const handleBack = (e) => {
         e.preventDefault();
-        const referrerURL = new URL(document.referrer);
-        if (referrerURL.origin === window.location.origin) {
-            navigate(referrerURL.pathname + referrerURL.search);
-        } else {
-            window.location.href = document.referrer;
-        }
+        navigate("/");
     };
 
     const handleError = (error) => {
