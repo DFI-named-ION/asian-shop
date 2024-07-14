977bc1fd2919fa7d62df291aca32654a73a5c476
DFI-named-ION <larityandselnter@gmail.com>
2024-07-14 06:56:32 -0400

    sessions + error + small menu

--- src/component/providers/AuthProvider.js
+++ src/component/providers/AuthProvider.js
@@ -6,30 +6,38 @@ export const AuthContext = React.createContext();
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [pending, setPending] = useState(true);
+    const [axiosError, setAxiosError] = useState({short: "", long: ""});
 
     const fetchUserData = async (fields) => {
         setPending(true);
         try {
-            const response = await axios.get(`${process.env.REACT_APP_WEB_API_BASE_URL}/Auth/requestData?fields=${fields}`);
+            const response = await axios.get(`${process.env.REACT_APP_WEB_API_BASE_URL}/Auth/fetchData?fields=${fields}`);
             setUser(response.data.data);
         } catch (err) {
-            console.error(err);
+            if (err.response && err.response.data && err.response.data === "Session Id is null") {
+                setUser(null);
+            } else {
+                handleError(err);
+            }
         } finally {
             setPending(false);
         }
     };
 
     useEffect(() => {
-        fetchUserData("fullName;isVerified;email;");
+        fetchUserData("displayName;isVerified;email;");
     }, []);
 
     const login = async (token) => {
         setPending(true);
         try {
+            setAxiosError({short: "", long: ""});
             await axios.post(`${process.env.REACT_APP_WEB_API_BASE_URL}/Auth/login`, { accessToken: token });
-            await fetchUserData("fullName;isVerified;email;");
+            await fetchUserData("displayName;isVerified;email;");
         } catch (err) {
-            console.error(err);
+            handleError(err);
+        }
+        finally {
             setPending(false);
         }
     };
@@ -37,10 +45,11 @@ export const AuthProvider = ({ children }) => {
     const logout = async () => {
         setPending(true);
         try {
-            await axios.post(`${process.env.REACT_APP_WEB_API_BASE_URL}/Auth/logout`);
+            setAxiosError({short: "", long: ""});
+            await axios.get(`${process.env.REACT_APP_WEB_API_BASE_URL}/Auth/logout`);
             setUser(null);
         } catch (err) {
-            console.error(err);
+            handleError(err);
         } finally {
             setPending(false);
         }
@@ -48,16 +57,26 @@ export const AuthProvider = ({ children }) => {
 
     const requestData = async (fields) => {
         try {
-            const response = await axios.get(`${process.env.REACT_APP_WEB_API_BASE_URL}/Auth/requestData?fields=${fields}`);
+            setAxiosError({short: "", long: ""});
+            const response = await axios.get(`${process.env.REACT_APP_WEB_API_BASE_URL}/Auth/fetchData?fields=${fields}`);
             setUser(prevUser => ({ ...prevUser, ...response.data.data }));
         } catch (err) {
-            console.error(err);
-            throw err;
+            handleError(err);
+        }
+    };
+
+    const handleError = (error) => {
+        if (error.response) {
+            setAxiosError({short: "Server error", long: "Internal server error occurred. Please try again later."});
+        } else if (error.request) {
+            setAxiosError({short: "Network error", long: "Network error. Please check your internet connection."});
+        } else {
+            setAxiosError({short: "Unexpected error", long: "An unexpected error occurred. Please try again."});
         }
     };
 
     return (
-        <AuthContext.Provider value={{ user, pending, login, logout, requestData }}>
+        <AuthContext.Provider value={{ user, pending, login, logout, requestData, axiosError }}>
             {pending ? <div className="text-center m-5 h-100">Loading...</div> : children}
         </AuthContext.Provider>
     );
